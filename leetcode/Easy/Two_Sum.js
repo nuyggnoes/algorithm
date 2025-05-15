bef19afb8824c763b332b838bf84b0f555f73cc2
임성균 <97024187+nuyggnoes@users.noreply.github.com>
2025-05-15 19:30:51 +0900

    leetcode algorithm

---
+++ leetcode/Easy/Two_Sum.js
@@ -0,0 +1,11 @@
+const twoSum = function (nums, target) {
+    const map = new Map();
+
+    for (let i = 0; i < nums.length; i++) {
+        const diff = target - nums[i];
+        if (map.has(diff)) {
+            return [map.get(diff), i];
+        }
+        map.set(nums[i], i);
+    }
+};
